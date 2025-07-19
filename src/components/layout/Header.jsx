185d473c70fd68f98417553bc8d38870a6d48d96
Athenkosi Magada <magadaathenkosi@gmail.com>
2025-07-19 10:45:51 +0200

    feat(update): update header.tsx
    - update header logo and links
    - updater header cart logo
    - update colors used.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,35 +1,36 @@
 import { Link, useLocation } from "react-router-dom";
+import { LuShoppingCart } from "react-icons/lu";
 
 const Header = () => {
   const location = useLocation();
 
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">
-        <Link to="/app/products">My E-Commerce</Link>
-      </h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Link
-              to="/app/products"
-              className={`hover:text-blue-300 ${
-                location.pathname === "/app/products" ? "text-blue-300" : ""
-              }`}
-            >
-              Products
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Link
-        to="/app/cart"
-        className={`hover:text-blue-300 ${
-          location.pathname === "/app/cart" ? "text-blue-300" : ""
-        }`}
-      >
-        Cart
-      </Link>
+    <header className="bg-primary">
+      <div className="max-w-[1440px] mx-auto px-8 text-white p-4 flex justify-between items-center">
+        <h1 className="text-xl font-bold">
+          <Link className="flex items-center gap-6" to="/app/products">
+            <div>Logo</div>
+            <div className="flex flex-col">
+              <span className="text-[18px]">Paradise Nursery</span>
+              <span className="text-sm font-normal italic">
+                Where Green Meets Serenity
+              </span>
+            </div>
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/app/products" className="text-white">
+                Plants
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Link to="/app/cart" className="">
+          <LuShoppingCart />
+        </Link>
+      </div>
     </header>
   );
 };
