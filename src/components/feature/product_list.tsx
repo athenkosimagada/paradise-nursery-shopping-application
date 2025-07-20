f0052f50ae9dc7583373c1ffde198dbc7d4bd96c
Athenkosi Magada <magadaathenkosi@gmail.com>
2025-07-20 20:18:34 +0200

    added some stuff for product

--- src/components/feature/product_list.tsx
+++ src/components/feature/product_list.tsx
@@ -1,11 +1,12 @@
 import { IMAGE_MAP } from "@/constants";
 import plantService from "@/services/plant_service";
 import { useEffect, useState } from "react";
+import { Button } from "@/components";
 
 const PRODUCT_FEATURES = [
   "Air Purifying Plants",
   "Aromatic Fragrant Plants",
-  "ow Maintenance Plants",
+  "Low Maintenance Plants",
   "Medicinal Plants",
   "Edible Plants",
 ];
@@ -24,28 +25,33 @@ const ProductList = () => {
   return (
     <div className="flex flex-col gap-4">
       {PRODUCT_FEATURES.map((feature) => (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 mb-12">
           <div className="w-max">
             <div className="h-[4px] bg-primary/60 rounded"></div>
             <h2 className="font-bold text-2xl">{feature}</h2>
             <div className="h-[4px] bg-primary/60 rounded"></div>
           </div>
 
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-4 gap-4 mt-4">
             {plants
               .filter(
                 (u) =>
                   u.commonFeature.toLowerCase() == feature.toLocaleLowerCase()
               )
               .map((plant) => (
-                <div>
-                  <p>{plant.name}</p>
+                <div className=" bg-primary/40 p-4 rounded-sm flex flex-col justify-center items-center">
+                  <p className="font-medium mb-2 text-center text-[18px]">
+                    {plant.name}
+                  </p>
                   <img
-                    className="max-w-full aspect-square"
+                    className="max-w-full aspect-square rounded-sm object-cover object-center"
                     src={IMAGE_MAP[plant.imageThumbnail]}
                     alt={plant.name}
                   />
                   <p>{plant.description}</p>
+                  <Button className="cursor-pointer mt-4 flex justify-self-end">
+                    Add to cart
+                  </Button>
                 </div>
               ))}
           </div>
